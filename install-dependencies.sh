dd5397c9dbba78dfe83a7f04328f12f9164f9ca8
Diegton Rodrigues <diegton@gmail.com>
2023-06-20 22:27:52 -0300

    fix install dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,4 @@
-git clone --no-checkout https://github.com/ColorlibHQ/gentelella.git  temp
+git clone https://github.com/ColorlibHQ/gentelella.git  temp
 mv temp ./src/main/resources/static/gentelella
 rm -R -f temp
 rm -R -f ./src/main/resources/static/gentelella/.git
