833b72af0960e44b2e450dfd7775c6186578728d
Diegton Rodrigues <diegton@gmail.com>
2023-06-23 18:20:03 -0300

    indexController

--- src/main/java/com/dehhvs/personalfinance/controllers/IndexController.java
+++ src/main/java/com/dehhvs/personalfinance/controllers/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexCOntroller {
+public class IndexController {
 
     @RequestMapping("/")
     public String index(ModelMap model) {
