a3968cda2885734bd8ff340ea13b971908a6aaae
Diegton Rodrigues <diegton@gmail.com>
2023-06-25 11:03:09 -0300

    variabes special character

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 # docker buildx build -f Production.Dockerfile -t diegton/personal-finances-spring .
+sudo rm -R ./target
+docker-compose -f deploy-docker-compose.yml up java
 docker buildx build --platform linux/arm64 -f Production.Dockerfile -t diegton/personal-finances-spring .
 docker push diegton/personal-finances-spring:latest
+sudo rm -R ./target
